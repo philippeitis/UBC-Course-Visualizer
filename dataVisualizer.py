fcca71f5c649d16a1ca213d6231cc1e330e32723
philippeitis <33013301+philippeitis@users.noreply.github.com>
2018-07-10 13:54:46 -0700

    Cleaning
    Re-organized the data visualization bits, moved a few more lines of codes into functions, removed the demo plotter, removed redundant code. Also added some sample images, and made the data visualization functions into real functions that can now accept any course code and pre-req description. So long as it doesn't mention percentages or non course code bits.
    I'm planning on adding support for percentages, non course code bits, and the ability to fetch pre-requisites externally. The only issue is that beautiful soup currently returns the pre-requisite paragraph with incorrect formatting, which makes using the data tricky.

--- dataVisualizer.py
+++ dataVisualizer.py
@@ -20,7 +20,8 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  
-#  
+#
+  
 class node():
     def __init__ (self,title):
         self.title = title
@@ -35,7 +36,10 @@ class node():
     
     def get_types():
         return(self.types)
-
+        
+    def get_type(self):
+		return(self.type)
+		
     def set_branches(self,branches):
         for branch in branches:
             self.branches.append(branch)
@@ -52,7 +56,7 @@ class node():
     def set_parent_node(self,parentNode):
         self.parentNode = parentNode
 
-    def get_branches():
+    def get_branches(self):
         return self.branches
 
     def switch_to_child_node(self,title):
@@ -98,31 +102,26 @@ def navigateTree(parentNode):
     
     # Goes through all the branches of the parent node recursively
     # Probably recursively
-
-    if parentNode.get_branch_num() == 0:
-        return(False)
-
-    else:
-        for branch in parentNode.branches:
-            branchParent = branch.get_parent_node()
-            if branchParent:
-                if branch.type != 'CHILD':
-                    print(branchParent.title + "->" + branch.title)
-                    print("Choose " + str(branch.nchoose) + " of:")
-                else:
-                    print(branch.title)
-                navigateTree(branch)
+    
+	for branch in parentNode.get_branches():
+		if branch.get_type() != 'CHILD':
+			print(parentNode.title + "->" + branch.title)
+			print("Choose " + str(branch.nchoose) + " of:")
+		else:
+			print(branch.title)
+		navigateTree(branch)
 
 def initializeDrawTree(parentNode):
     from graphviz import Digraph
-    dot = Digraph(comment='CPSC340')
-    dot.attr(compound='true',label='', fontcolor='white')
-    dot.attr()
-    dot.node_attr.update(color='#0D47CE', style='filled',shape='box',fontcolor='black')
-    dot.node('CPSC340',color='#71ABFF',style='filled',fontcolor='black')
+    dot = Digraph(comment=parentNode.title)
+    dot.attr(compound='true',label='')
+    
+    dot.node('CPSC340',color='#71ABFF',style='filled')
+    dot.node_attr.update(color='#0D47CE', style='filled',shape='box')
+    
     graph = drawTree(dot,parentNode)
     
-    graph.render('test-output/CPSC340.gv', view=True)  # doctest: +SKIP
+    graph.render('test-output/' + parentNode.title + '.gv', view=True)
     return dot
 
 def drawTree(graph,parentNode,num=0):
@@ -149,78 +148,82 @@ def drawTree(graph,parentNode,num=0):
     return graph
 
 # Decent code, needs improvement   
-def coursePreReqTreeGen():
-    preReqs = 'One of MATH 152, MATH 221, MATH 223 and one of MATH 200, MATH 217, MATH 226, MATH 253, MATH 263 and one of STAT 200, STAT 203, STAT 241, STAT 251, COMM 291, ECON 325, ECON 327, PSYC 218, PSYC 278, PSYC 366, MATH 302, STAT 302, MATH 318, BIOL 300; and either (a) CPSC 221 or (b) all of CPSC 260, EECE 320 and one of CPSC 210, EECE 210, EECE 309.'
-
-    preReqKWList = ["one of","two of", "three of", "either", "or", "all of", "and"]
-    newBranchKW = "and"
-    subBranchKW = "either"
-    escapeBranchKW = "or"
-
+def coursePreReqTreeGen(courseCode,preReqs=False):
+    print(preReqs)
+	
+    preReqKWList = ("one of","two of", "three of", "either", "or", "all of", "and")
+    
+    # To make expanding these easier, I have left them as lists in case new key words appear
+    newBranchKW = ("and")
+    subBranchKW = ("either")
+    escapeBranchKW = ("or")
     nchooseDict = {"one of":1,"two of":2,"three of":3,"all of":'all'}
 
     preReqList = []
-    preReqsx = preReqs.split(" ")
-    for i in range(1,len(preReqsx)):
-        department = preReqsx[i-1]
-        code = preReqsx[i]
+    preReqs = preReqs.split(" ")
+    
+    for i in range(1,len(preReqs)):
+        department = preReqs[i-1]
+        code = preReqs[i]
 
-        
         if department.lower() + ' ' + code in preReqKWList:
             preReqList.append(department + ' ' + code)
 
         elif code in preReqKWList:
             preReqList.append(code)
 
-        courseCode = True
+        preReq = True
         
         for letter in department:
             try:
-                letter = float(letter)
-                courseCode = False
+				#Check if it's a number: if it is, then not a pre-req
+                letter = int(letter)
+                preReq = False
                 break
-            except ValueError:    
+            except ValueError:
+				# If it isn't a number, check if it's lower case.
                 if letter.islower():
-                    courseCode = False
+                    preReq = False
                     break
 
-        if courseCode:
-            courseCode = str(department) + ' ' + str(code)
+        if preReq:
+            preReq = str(department) + ' ' + str(code)
             for character in (",",";","."):
-                courseCode = courseCode.strip(character)
-            preReqList.append(courseCode)
+                preReq = preReq.strip(character)
+            preReqList.append(preReq)
 
     # Initializing tree with parent node and name
-    parentNode = node('CPSC340')
+    parentNode = node(courseCode)
     parentNode.set_type('ROOT')
 
     # Creating branch (node) for first pre-reqs, setting active node (node being operated on)
     # Setting information
-    parentNode.set_branch(node("Initial"))
-    activeNode = parentNode.get_last_branch()
-    activeNode.set_parent_node(parentNode)
+    activeNode = parentNode.switch_to_child_node("Initial")
     activeNode.set_nchoose(nchooseDict[preReqList[0].lower()])
 
-    
     i = 0
    
     # This code exists solely for the initial node
     for string in preReqList[1:]:
         i+=1
-        if string.lower() in newBranchKW:
+        string = string.lower()
+
+        if string in newBranchKW:
             break
-        elif string.lower() in subBranchKW:
+        elif string in subBranchKW:
             break
-        elif string.lower() in escapeBranchKW:
+        elif string in escapeBranchKW:
             break
-        currNode = activeNode.switch_to_child_node(string)
+        currNode = activeNode.switch_to_child_node(string.upper())
         currNode.set_type('CHILD')
     a = 0
     b = 0
 
     # Traverses the list and creates nodes with branches for each pre-requisite
     for string in preReqList[i:]:
-        if string.lower() in newBranchKW:
+		
+        string = string.lower()
+        if string in newBranchKW:
             a += 1
 
             # This creates a new branch from the parent of the branch and switches to the new branch if 'and' is detected: creates a new branch in parallel to the current one and switches to it
@@ -229,14 +232,14 @@ def coursePreReqTreeGen():
             activeNode = activeNode.switch_to_child_node(string+str(a))
 
 
-        elif string.lower() in subBranchKW:
+        elif string in subBranchKW:
 
             # This creates a new branch from the active branch and switches to the new branch if 'either' is detected: creates a new branch below the current one and switches to it
 
             activeNode = activeNode.switch_to_child_node(string)
             activeNode.set_nchoose(1)
 
-        elif string.lower() in escapeBranchKW:
+        elif string in escapeBranchKW:
 
             # This creates a new branch from the parent of the active branch, switches to it, and creates a new branch from the new branch if 'or' is detected
 
@@ -252,50 +255,21 @@ def coursePreReqTreeGen():
             activeNode.set_nchoose(1)
 
 
-        elif string.lower() in nchooseDict.keys():
-            activeNode.set_nchoose(nchooseDict[string.lower()])
+        elif string in nchooseDict.keys():
+            activeNode.set_nchoose(nchooseDict[string])
 
         else:
-            currNode = activeNode.switch_to_child_node(string)
+            currNode = activeNode.switch_to_child_node(string.upper())
             currNode.set_type('CHILD')
 
 
     navigateTree(parentNode)
     initializeDrawTree(parentNode)
+    
 def main(args):
     return 0
 
-def demoPlotter():
-    
-    from graphviz import Digraph
-
-    dot = Digraph(comment='CPSC340')
-    dot  #doctest: +ELLIPSIS
-    dot.attr(compound='true')
-    dot.attr(bgcolor='grey', label='CPSC340', fontcolor='white')
-
-    with dot.subgraph(name = 'cluster2') as Z:
-        Z.attr(bgcolor='grey', label='Either (a)', fontcolor='white')
-        Z.node('H','CPSC 221')
-        with Z.subgraph(name = 'cluster0') as E:
-            E.attr(bgcolor='grey', label=' or (b)', fontcolor='white')
-            E.node('F','One of \n CPSC 210, \n EECE 210, \n EECE 309')
-            E.node('G','All of \n CPSC 260, \n EECE 320')
-
-    with dot.subgraph(name = 'cluster1') as dot2:
-        dot.attr(bgcolor='grey', label='', fontcolor='white')
-        dot2.node('A', 'CPSC340')
-        dot2.node('B', 'One of \n MATH 152, \n MATH 221, \n MATH 223')
-        dot2.node('C', 'One of \n MATH 200, \n MATH 217, \n MATH 226, \n MATH 253, \n MATH 263')
-        dot2.node('D', 'One of \n STAT 200, \n STAT 203, \n STAT 241, \n STAT 251, \n COMM 291, \n ECON 325, \n ECON 327, \n PSYC 218, \n PSYC 278, \n PSYC 366, \n MATH 302, \n STAT 302, \n MATH 318, \n BIOL 300')
-
-    dot.edge('F','H',ltail='cluster0',lhead='cluster2')
-    dot.edge('H','F',ltail='cluster2',lhead='cluster0')
-    dot.edge('H','A',ltail='cluster2',lhead='cluster0')
-    dot.edges(['BA', 'CA', 'DA'])
-    dot.render('test-output/CPSC340.gv', view=True)  # doctest: +SKIP
-
 if __name__ == '__main__':
     import sys
-    coursePreReqTreeGen()
+    coursePreReqTreeGen('CPSC340','One of MATH 152, MATH 221, MATH 223 and one of MATH 200, MATH 217, MATH 226, MATH 253, MATH 263 and one of STAT 200, STAT 203, STAT 241, STAT 251, COMM 291, ECON 325, ECON 327, PSYC 218, PSYC 278, PSYC 366, MATH 302, STAT 302, MATH 318, BIOL 300; and either (a) CPSC 221 or (b) all of CPSC 260, EECE 320 and one of CPSC 210, EECE 210, EECE 309.')
     sys.exit(main(sys.argv))
